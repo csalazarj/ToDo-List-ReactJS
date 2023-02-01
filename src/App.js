6e4441120de12d26f64f57189f9126197d9a6fc1
csalazarj <csalazar.red@gmail.com>
2023-02-01 17:20:03 -0500

    delete and complete TODOs parcially implemented

--- src/App.js
+++ src/App.js
@@ -33,6 +33,19 @@ function App() {
         })
     }
 
+    const completeTodo = (text) => {
+        const todoIndex = todos.findIndex((todo) => todo.text === text)
+        const newTodos = [...todos]
+        newTodos[todoIndex].completed = true
+        setTodos(newTodos)
+    }
+    const deleteTodo = (text) => {
+        const todoIndex = todos.findIndex((todo) => todo.text === text)
+        const newTodos = [...todos]
+        newTodos.splice(todoIndex, 1)
+        setTodos(newTodos)
+    }
+
     return (
         <React.Fragment>
             <TodoCounter total={totalTodos} completed={completedTodos} />
@@ -48,6 +61,8 @@ function App() {
                         key={todo.text}
                         text={todo.text}
                         completed={todo.completed}
+                        onComplete={() => completeTodo(todo.text)}
+                        onDelete={() => deleteTodo(todo.text)}
                     />
                 ))}
             </TodoList>
--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -2,19 +2,13 @@ import React from "react"
 import "./TodoItem.css"
 
 function TodoItem(props) {
-    const onComplete = () => {
-        alert(" Ya completaste el ToDo " + props.text)
-    }
-    const onDelete = () => {
-        alert(" Borraste el ToDo " + props.text)
-    }
     return (
         <li className="TodoItem">
             <span
                 className={`Icon Icon-check ${
                     props.completed && "Icon-check--active"
                 }`}
-                onClick={onComplete}
+                onClick={props.onComplete}
             >
                 √
             </span>
@@ -26,7 +20,7 @@ function TodoItem(props) {
             >
                 {props.text}
             </p>
-            <span className="Icon Icon-delete" onClick={onDelete}>
+            <span className="Icon Icon-delete" onClick={props.onDelete}>
                 X
             </span>
         </li>
