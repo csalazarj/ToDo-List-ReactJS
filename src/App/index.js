9e6ef22f811ea6788e7eefde81ee3aac98371bc6
csalazarj <csalazar.red@gmail.com>
2023-02-02 15:04:58 -0500

    Changes made to the TODOs are now saved in LocalStorage

--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,26 @@
 import React from "react"
 import { AppUI } from "./AppUI"
 
-const defaultTodos = [
-    { text: "Cortar cebolla", completed: true },
-    { text: "Tomar el curso de intro a react", completed: true },
-    { text: "Comer chocolate", completed: false },
-    { text: "Comer pollo", completed: false },
-    { text: "Hacer tares", completed: false },
-]
+// const defaultTodos = [
+//     { text: "Cortar cebolla", completed: true },
+//     { text: "Tomar el curso de intro a react", completed: true },
+//     { text: "Comer chocolate", completed: false },
+//     { text: "Comer pollo", completed: false },
+//     { text: "Hacer tares", completed: false },
+// ]
 
 function App() {
-    const [todos, setTodos] = React.useState(defaultTodos)
+    const localStorageTodos = localStorage.getItem("TODOS_V1")
+    let parsedTodos
+
+    if (!localStorageTodos) {
+        localStorage.setItem("TODOS_V1", JSON.stringify([]))
+        parsedTodos = []
+    } else {
+        parsedTodos = JSON.parse(localStorageTodos)
+    }
+
+    const [todos, setTodos] = React.useState(parsedTodos)
     const [searchValue, setSearchValue] = React.useState("")
 
     const completedTodos = todos.filter((todo) => !!todo.completed).length
@@ -28,17 +38,23 @@ function App() {
         })
     }
 
+    const saveTodos = (newTodos) => {
+        const stringifiedTodos = JSON.stringify(newTodos)
+        localStorage.setItem("TODOS_V1", stringifiedTodos)
+        setTodos(newTodos)
+    }
+
     const completeTodo = (text) => {
         const todoIndex = todos.findIndex((todo) => todo.text === text)
         const newTodos = [...todos]
         newTodos[todoIndex].completed = true
-        setTodos(newTodos)
+        saveTodos(newTodos)
     }
     const deleteTodo = (text) => {
         const todoIndex = todos.findIndex((todo) => todo.text === text)
         const newTodos = [...todos]
         newTodos.splice(todoIndex, 1)
-        setTodos(newTodos)
+        saveTodos(newTodos)
     }
 
     return (
