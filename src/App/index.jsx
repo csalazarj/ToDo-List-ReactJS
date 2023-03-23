ae954dac8c69e0160823bf5c6d383eae74d24e29
csalazarj <csalazar.red@gmail.com>
2023-03-22 19:59:40 -0500

    useContext replaced by components composition in a cuntom hook useTodos

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,6 +1,16 @@
 import React from "react"
-import { AppUI } from "./AppUI"
-import { TodoProvider } from "../TodoContext"
+import { useTodos } from "./useTodos"
+import { TodoCounter } from "../TodoCounter"
+import { TodoSearch } from "../TodoSearch"
+import { TodoItem } from "../TodoItem"
+import { TodoForm } from "../TodoForm"
+import { TodoList } from "../TodoList"
+import { TodoHeader } from "../TodoHeader"
+import { CreateTodoButton } from "../CreateTodoButton"
+import { Modal } from "../Modal"
+import { TodosLoading } from "../TodosLoading"
+import { EmptyTodos } from "../EmptyTodos"
+import { TodosError } from "../TodosError"
 
 // const defaultTodos = [
 //     { text: "Cortar cebolla", completed: true },
@@ -11,11 +21,57 @@ import { TodoProvider } from "../TodoContext"
 // ]
 
 function App() {
+    const {
+        error,
+        loading,
+        searchedTodos,
+        completeTodo,
+        deleteTodo,
+        openModal,
+        setOpenModal,
+        totalTodos,
+        completedTodos,
+        searchValue,
+        setSearchValue,
+        addTodo,
+    } = useTodos()
+
     return (
-        <TodoProvider>
-            <AppUI />
-        </TodoProvider>
-        
+        <React.Fragment>
+            <TodoHeader>
+                <TodoCounter
+                    totalTodos={totalTodos}
+                    completedTodos={completedTodos}
+                />
+
+                <TodoSearch
+                    searchValue={searchValue}
+                    setSearchValue={setSearchValue}
+                />
+            </TodoHeader>
+
+            <TodoList>
+                {error && <TodosError error={error} />}
+                {loading && <TodosLoading />}
+                {!loading && !searchedTodos.length && <EmptyTodos />}
+                {searchedTodos.map((todo) => (
+                    <TodoItem
+                        key={todo.text}
+                        text={todo.text}
+                        completed={todo.completed}
+                        onComplete={() => completeTodo(todo.text)}
+                        onDelete={() => deleteTodo(todo.text)}
+                    />
+                ))}
+            </TodoList>
+
+            {!!openModal && (
+                <Modal>
+                    <TodoForm addTodo={addTodo} />
+                </Modal>
+            )}
+            <CreateTodoButton setOpenModal={setOpenModal} />
+        </React.Fragment>
     )
 }
 
